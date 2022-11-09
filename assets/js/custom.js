a9b0ff0819a8f87e20d031948653b0662bd4839c
Pununuk <pununuk@MacBook-Pro-de-Pununuk.local>
2022-11-09 17:04:38 -0300

    octubre

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,6 @@
 const data = {
     datasets: [{
-        data: [89.69, 1.59, 8.72],
+        data: [67.14, 0.70, 32.15],
         backgroundColor: [
             'rgb(36, 65, 84)',
             'rgb(157, 201, 201)',
